b9b2db9bb5e1dc19bfbda0c73c1da503f01aac5f
yoshi <joshuataylor574@gmail.com>
2024-11-10 20:17:58 +0000

    update version, add requirement for setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='MangaSplitter',
-    version='0.0.1',
+    version='0.0.2',
     entry_points={
         'console_scripts': [
             'manga_split = manga_split:main'
